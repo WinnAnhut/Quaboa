32f52dd22008c36ce23e2333ba441e9ec8548ed2
Winn Anhut <anhutwinn@gmail.com>
2017-02-24 22:16:16 -0500

    My first commit

--- Quaboa/ViewController.swift
+++ Quaboa/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    var TapCount = 0
+
+    @IBAction func buttontap1(_ sender: AnyObject) {
+        label1.text = "UR A PUSSY!"
+        TapCount = TapCount + 1
+        print(TapCount)
+        
+        if TapCount >= 10 {
+            label1.text = "Less of a pussy."
+        }
+        if TapCount >= 20 {
+            label1.text = "not a pussy."
+        }
+        if TapCount >= 30 {
+            label1.text = "bueno trabaja!"
+        }
+    }
+    
+// Winn ur a god
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
